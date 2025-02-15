db8169296e9c9245712b1fef0bdbbef640b563d6
JennyDonghe <hd2365@nyu.edu>
2025-02-15 00:16:16 -0500

    Fixed Prject1 folder name

---
+++ project1/Project1/SDLSimple_1/SDLSimple_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDLSimple_1
+//
+//  Created by Jenny's Mac on 1/31/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
