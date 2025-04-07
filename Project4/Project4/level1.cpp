6faeff1bd694570e49930134aeecc0f4e8888361
JennyDonghe <hd2365@nyu.edu>
2025-04-07 18:08:56 -0400

    final project4

--- Project4/Project4/level1.cpp
+++ Project4/Project4/level1.cpp
@@ -1,3 +1,12 @@
+/**
+* Author: [JennyDong]
+* Assignment: Rise of the AI
+* Date due: 2025-04-05, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 #include "level1.h"
 #include "utility.h"
 #include <iostream>
